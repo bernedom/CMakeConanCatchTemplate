5812b915e9ae4fd832a9a9825589ec22916bfac5
Dominik Berner <dominik.berner@wingtra.com>
2024-12-04 16:55:27 +0100

    Add example implementation

---
+++ src/example.cpp
@@ -0,0 +1,3 @@
+#include "example.h"
+
+bool is_even(int n) { return n % 2 == 0; }
--- test/src/example_test.cpp
+++ test/src/example_test.cpp
@@ -1,7 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <example.h>
+
 TEST_CASE("Example test", "[example]") {
     REQUIRE(1 == 1);
     REQUIRE(true);
 }
 
+TEST_CASE("Test is_even", "[example]") {
+  REQUIRE(is_even(2));
+  REQUIRE_FALSE(is_even(3));
+}
